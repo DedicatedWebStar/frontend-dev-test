a3bf8bad4d5db2b758477e33503dd95178896e14
YegorYanushkevich <yegoryanushkevich@gmail.com>
2018-07-26 10:26:08 +0200

    Updated secure page

--- src/app/home/components/secure/secure.component.ts
+++ src/app/home/components/secure/secure.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ApiService } from '../../../core/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-secure',
   templateUrl: './secure.component.html',
-  styleUrls: ['./secure.component.scss']
+  styleUrls: ['./secure.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SecureComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  logout() {
+    this.apiService.reset();
+    const route = this._router;
+    setTimeout(function() {
+      route.navigate(['/auth/login']);
+      window.location.reload();
+    }, 1500 );
+  }
 }
